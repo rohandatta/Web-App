8bcb71f34bef820a6ec169a21d566c98a3eb5e2f
rohan_datta <65494477+rohandatta@users.noreply.github.com>
2021-04-13 18:00:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='Heart Disease Prediction',
     version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    description='A web app to predict heart disease based on user information',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Rohan Datta',
+    author_email='dattarohan4@gmail.com',
+    url='https://github.com/rohandatta/Web-App',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
